88b3a4328640e8329e806e6b3662ef627fc113f5
devcosmos <va-litv@yandex.ru>
2026-05-23 19:36:37 +0300

    refactor: remove standalone output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./app/src/core/i18n/i18n.ts');
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   basePath: '/schedule',
   images: {
     remotePatterns: [
