90059de7f37caa7b0998f96520b4ce8b8b495a41
devcosmos <va-litv@yandex.ru>
2026-05-27 22:04:34 +0300

    feat: add new icons for BookmarkFolder, MarkOutline, and Stars components

---
+++ app/src/components/icon/bold/mark-outline.tsx
@@ -0,0 +1,20 @@
+import IconWrapper from '@components/icon/icon-wrapper';
+
+export default function MarkOutline({ ...rest }: React.SVGAttributes<SVGElement>) {
+  return (
+    <IconWrapper width="24" height="24" {...rest}>
+      {/* <path d="M0 0h24v24H0z" fill="none" /> */}
+      <g strokeWidth="1.5">
+        <path
+          className="stroke-tg-link-color fill-transparent transition-all duration-300 group-[.scaling]:fill-tg-link-color"
+          d="M21 16.09v-4.992c0-4.29 0-6.433-1.318-7.766C18.364 2 16.242 2 12 2S5.636 2 4.318 3.332S3 6.81 3 11.098v4.993c0 3.096 0 4.645.734 5.321c.35.323.792.526 1.263.58c.987.113 2.14-.907 4.445-2.946c1.02-.901 1.529-1.352 2.118-1.47c.29-.06.59-.06.88 0c.59.118 1.099.569 2.118 1.47c2.305 2.039 3.458 3.059 4.445 2.945c.47-.053.913-.256 1.263-.579c.734-.676.734-2.224.734-5.321Z"
+        />
+        <path
+          strokeLinecap="round"
+          d="M15 6H9"
+          className="stroke-tg-link-color transition-all duration-300 group-[.scaling]:stroke-tg-section-bg-color"
+        />
+      </g>
+    </IconWrapper>
+  );
+}
