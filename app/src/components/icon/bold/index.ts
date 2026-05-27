e7c5d9d51a286df2426f2aaeda6546727c8d9245
devcosmos <va-litv@yandex.ru>
2026-05-27 09:16:39 +0300

    feat: add Filter and MinimalisticMagnifierLinear icon components

--- app/src/components/icon/bold/index.ts
+++ app/src/components/icon/bold/index.ts
@@ -1,3 +1,5 @@
 export { default as Calendar } from './calendar';
 export { default as CircleUser } from './circle-user';
+export { default as Filter } from './filter';
 export { default as Mark } from './mark';
+export { default as Magnifier } from './minimalistic-magnifer-linear';
---
+++ app/src/components/icon/bold/minimalistic-magnifer-linear.tsx
@@ -0,0 +1,13 @@
+import IconWrapper from '@components/icon/icon-wrapper';
+
+export default function MinimalisticMagnifierLinear({ ...rest }: React.SVGAttributes<SVGElement>) {
+  return (
+    <IconWrapper width="24" height="24" {...rest}>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <g fill="none" stroke="currentColor" strokeWidth="1.5">
+        <circle cx="11.5" cy="11.5" r="9.5" />
+        <path strokeLinecap="round" d="m20 20l2 2" />
+      </g>
+    </IconWrapper>
+  );
+}
