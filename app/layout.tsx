51dc58311f5e11e04035471fe4f2f1779050a129
devcosmos <va-litv@yandex.ru>
2026-05-29 12:18:38 +0300

    feat: add Yandex Metrica component for analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Rubik } from 'next/font/google';
 
 import '@styles/globals.css';
 
+import YandexMetrica from '@components/yandex-metrica';
+
 import I18nProvider from '@core/i18n/provider';
 import TelegramProvider from '@core/telegram/provider';
 
@@ -27,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={rubik.className}>
+        <YandexMetrica />
         <I18nProvider>
           <TelegramProvider>{children}</TelegramProvider>
         </I18nProvider>
