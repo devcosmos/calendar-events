292367c55fc763e09ea3f2a87065ca727d534e13
devcosmos <va-litv@yandex.ru>
2026-06-05 09:49:25 +0300

    refactor: allow additional props in CalendarSliderContainer for better flexibility

--- app/src/components/calendar/calendar-slider-container.tsx
+++ app/src/components/calendar/calendar-slider-container.tsx
@@ -1,6 +1,9 @@
-export default function CalendarSliderContainer({ children }: React.PropsWithChildren) {
+export default function CalendarSliderContainer({
+  children,
+  ...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) {
   return (
-    <div className="flex-[0_0_100%] h-auto min-w-0 w-dvw">
+    <div className="flex-[0_0_100%] h-auto min-w-0 w-dvw" {...props}>
       <div className="h-full overflow-y-auto scrollbar px-3 pb-28 pt-16">{children}</div>
     </div>
   );
