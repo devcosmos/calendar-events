e6aa1d6074b5fdfd843463e382b90e9f807bee56
devcosmos <va-litv@yandex.ru>
2026-06-10 10:06:49 +0300

    fix: update TG_CHAT_NAME for production environment consistency

--- app/src/utils/api/endpoints.ts
+++ app/src/utils/api/endpoints.ts
@@ -10,7 +10,7 @@ export const FRONTEND_URL = 'https://swim-calendar-alpha.vercel.app';
 export const API_BACKEND_URL = 'https://swim-calendar-alpha.vercel.app/api';
 export const TG_BOT_NAME = isDev ? 'Skshfntorps_bot' : 'swim_calendar_bot';
 
-export const TG_CHAT_NAME = isDev ? 'test_schedule_chat' : 'swim_calendar_support';
+export const TG_CHAT_NAME = isDev ? 'test_schedule_chat' : 'swimcosmos';
 export const TG_CHANNEL_NAME = 'swim_calendar';
 export const TG_MINI_APP_NAME = isDev ? 'blablabla' : 'swim_calendar_app';
 
